5224f6cb9f68abd1e65f7f298612f71b89fe9158
patrick-204 <p.ohara921@gmail.com>
2024-04-19 21:49:13 -0400

    Add POST route handler to the tweet submission form. Add new-tweet CSS file for styling the entering and submission of tweets. Modify index HTML file to include the new-tweet CSS file styles.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// specify the DOM node to reference using the document.getElementById method and put that reference in a variable
+
+
 
