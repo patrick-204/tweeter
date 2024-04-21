5008c0abc90064da168f1d0f93a9d370ff073c31
patrick-204 <p.ohara921@gmail.com>
2024-04-20 20:31:15 -0400

    Fix createTweetElement function to create the article part of the DOM. Add renderTweets function for looping through the user tweets and appending them to the main element container.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,52 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  // Render tweets form
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  // Render tweets
+  const renderTweets = function(tweets) {
+    // Loop through tweets
+    for (const tweet of tweets) {
+      // Call createTweetElement function to create a tweet article element
+      const $tweetArticle = createTweetElement(tweet);
+
+      // Append the created tweet article element to the #tweets-container
+      $('.container').append($tweetArticle);
+    }
+  };
+
+  // Creat tweets
   const createTweetElement = function(tweet) {
-    // Article 
-    const $tweetArticle = $('main article');
-    $tweetArticle.addClass('tweet-article');
+    // Create tweet article element
+    const $tweetArticle = $('<article>').addClass('tweet-article');
 
     // Header 
-    const $header = $tweetArticle.find('header');
-    const $div = $header.find('div');
-    $div.addClass('tweeter-info');
-    const $img = $div.find('img');
-    $img.attr('src', tweet.user.avatars);
-    const $nameSpan = $div.find('span').eq(0); 
-    $nameSpan.text(tweet.user.name);
-    const $handleSpan = $header.find('span').eq(1); 
-    $handleSpan.text(tweet.user.handle);
+    const $header = $('<header>');
+    const $div = $('<div>').addClass('tweeter-info');
+    const $img = $('<img>').attr('src', tweet.user.avatars);
+    const $nameSpan = $('<span>').text(tweet.user.name);
+    const $handleSpan = $('<span>').text(tweet.user.handle);
+    $div.append($img, $nameSpan);
+    $header.append($div, $handleSpan);
 
     // Tweet/paragraph
-    const $tweetParagraph = $tweetArticle.find('p');
-    // Add the tweet content 
-    $tweetParagraph.text(tweet.content.text);
+    const $tweetParagraph = $('<p>').text(tweet.content.text);
 
     // Footer
-    const $footer = $tweetArticle.find('footer');
-    const $output = $footer.find('output');
-    $output.attr('name', 'datetime').text(tweet.created_at);
-    const $icons = $footer.find('span i');
-    $icons.eq(0).addClass('fa-brands fa-font-awesome');
-    $icons.eq(1).addClass('fa-solid fa-retweet');
-    $icons.eq(2).addClass('fa-solid fa-heart');
+    const $footer = $('<footer>');
+    const $output = $('<output>').attr('name', 'datetime').text(tweet.created_at);
+    const $span = $('<span>');
+    const $icon1 = $('<i>').addClass('fa fa-brands fa-font-awesome');
+    const $icon2 = $('<i>').addClass('fa fa-solid fa-retweet');
+    const $icon3 = $('<i>').addClass('fa fa-solid fa-heart');
+    $span.append($icon1, $icon2, $icon3);
+    $footer.append($output, $span);
+
+    // Append all elements in article to article
+    $tweetArticle.append($header, $tweetParagraph, $footer);
 
     return $tweetArticle;
   };
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-    }
-
-    $('main').append(createTweetElement(tweetData));
+  // Render the tweets with the fake data taken from initial-tweets.json
+  renderTweets(data);
 });
